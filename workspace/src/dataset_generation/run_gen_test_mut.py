b08e1550c86f0d1f719968c2ed253b8132e36c63
hslee1225 <rcmooac@gmail.com>
2024-03-07 10:36:38 +0900

    Add mutation select code and Closure diff

---
+++ workspace/src/dataset_generation/run_gen_test_mut.py
@@ -0,0 +1,21 @@
+import subprocess
+import pickle
+from tqdm import tqdm 
+
+# Define the parameters
+param0 = "Closure"
+param1 = "1"
+param2 = "newTS_300"
+param3 = "300"
+param4 = "0"
+
+file_path = "/root/workspace/src/select_dataset/Closure_mutants.pkl"
+with open(file_path, 'rb') as file:
+    diffs = pickle.load(file)
+target = [diff.split('_')[0] for diff in diffs]
+
+
+for i in tqdm(target):  
+    command = f"sh gen_test_mut.sh {param0} {param1} {i} {param2} {param3} {param4}"
+    subprocess.run(command, shell=True)  
+    
---
+++ workspace/src/select_dataset/select_mutation.py
@@ -0,0 +1,85 @@
+import sys
+sys.path.append('../')
+import os
+import argparse
+from utils.env import EvoD4jEnv
+import random
+import pickle
+
+def parse_diff_file(diff_path):
+    """Parse a diff file to extract starting line numbers of changes."""
+    with open(diff_path) as file:
+        for line in file:
+            if line.startswith('@@'):
+                parts = line.split(' ')
+                line_number = int(parts[2].split(',')[0][1:])  # Extract start line number
+    return line_number
+
+def randomly_select_one_value_per_key(dic):
+    selected = []
+    for key, value in dic.items():
+        selected.append(random.sample(value,1)[0])
+    return selected
+
+def select_random_diffs_per_line_number(diff_dir, infos):
+    cand = {} # key: line_number , value: idxs
+    for info in infos:
+        diff = info[:info.find('_')] + ".diff"
+        diff_path = os.path.join(diff_dir, diff)
+        
+        line_number = parse_diff_file(diff_path)
+        print(line_number)
+        if line_number not in cand:
+            cand[line_number] = [info]
+        else:
+            cand[line_number].append(info)
+    selected_elements = []
+    # Randomly select 10 elements in set if the length of set is over than 10
+    if len(cand) > 10:
+        selected_line_number = random.sample(list(cand.keys()), 10)
+        filtered_dict = {line_nubmer: cand[line_nubmer] for line_nubmer in selected_line_number}
+        selected_elements = randomly_select_one_value_per_key(filtered_dict)
+    else:
+        selected_elements = randomly_select_one_value_per_key(cand)
+    return selected_elements
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('project', type=str)
+    parser.add_argument('version', type=str)
+    args = parser.parse_args()
+
+    project = args.project
+    version = args.version
+    
+    env = EvoD4jEnv(project, version)
+
+    diff_dir = os.path.join(env.WORK_DIR, f"result/{project}-{version}f_mutated/git_diff")
+
+    diff_infos = filter(lambda x: x.endswith("info"), os.listdir(diff_dir))
+
+    
+    info_dic = {} # key: class, value: dictionary (key: method, value: infos)
+
+    for info in diff_infos:
+        contents = open(os.path.join(diff_dir,info)).read()
+        class_name = contents.split(":")[0].strip()
+        method_name = contents.split(":")[1].strip()
+        if class_name not in info_dic:
+            info_dic[class_name] = {method_name: [info]}
+        elif class_name in info_dic and method_name not in info_dic[class_name]:
+            info_dic[class_name][method_name] = [info]
+        elif class_name in info_dic and method_name in info_dic[class_name]:
+            info_dic[class_name][method_name].append(info)
+    
+    target_diffs = []
+    for class_name, method_dic in info_dic.items():
+        print("------------------------------------")
+        for method_name, infos in method_dic.items():
+            if len(infos) <= 10: 
+                target_diffs.extend(infos)
+            else: 
+                target_diffs.extend(select_random_diffs_per_line_number(diff_dir,infos))
+    
+    with open(f'{project}_mutants.pkl','wb') as f:
+        pickle.dump(target_diffs, f)
