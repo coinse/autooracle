1e5b426768775d243dbcb9716d10837eece2866f
hslee1225 <rcmooac@gmail.com>
2024-03-12 11:14:50 +0900

    node2

--- workspace/src/dataset_generation/run_dataframe.py
+++ workspace/src/dataset_generation/run_dataframe.py
@@ -1,20 +1,15 @@
 import subprocess
+from tqdm import tqdm
 import pickle
-from tqdm import tqdm 
 
-# Define the parameters
 param0 = "Time"
 param1 = "1"
 param2 = "newTS_300"
-param3 = "300"
-param4 = "0"
 
 file_path = "/root/workspace/src/select_dataset/Time_mutants.pkl"
 with open(file_path, 'rb') as file:
     diffs = pickle.load(file)
 target = [diff.split('_')[0] for diff in diffs]
 
-for i in tqdm(target[1100:1130]):  
-    command = f"sh gen_test_mut.sh {param0} {param1} {i} {param2} {param3} {param4}"
-    subprocess.run(command, shell=True)  
-    
+for i in tqdm(target): 
+    subprocess.run(["python", "dataframe.py", param0, param1, "-idx", i, "-i", param2])
